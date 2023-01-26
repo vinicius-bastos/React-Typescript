6de14e38fa8a1bc35fbb80b71f0289293e026919
vinicius-bastos <viniciusbastos45@gmail.com>
2023-01-26 14:18:56 -0300

    feature: how to implement forwardRef

--- Curso de react/src/app/pages/dashboard/Dashboard.tsx
+++ Curso de react/src/app/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,19 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 export const Dashboard = () => {
+
+    const counterRef = useRef(0);
+
     return (
         <div>
             <p>Dashboard</p>
+
+            <p>Contador: {counterRef.current}</p>
+
+            <button onClick={() => counterRef.current++}>Somar</button>
+            <button onClick={() => console.log(counterRef.current)}>Logar</button>
+
             <Link to={"/entrar"}>Login</Link>
         </div>
     );
--- Curso de react/src/app/pages/login/Login.tsx
+++ Curso de react/src/app/pages/login/Login.tsx
@@ -1,28 +1,52 @@
-import { useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
+import { InputLogin } from "./components/InputLogin";
 
 export const Login = () => {
 
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
+
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
-   
-    const handleEntrar = () => {
+
+    const handleEntrar = useCallback(() => {
         console.log(email)
         console.log(senha)
-    }
+    }, [email, senha]);
+
+    const emailLength = useMemo(() => {
+        return email.length * 1000;
+    }, [email.length]);
+
     return (
         <div>
             <form>
 
-                <label>
-                    <span>Email</span>
-                    <input value={email} onChange={e => setEmail(e.target.value)}/>
-                </label>
+                <p>Quantidade de caracteres no email: {emailLength}</p>
 
-                <label>
+                <InputLogin
+                 label="Email"
+                 value={email}
+                 onChange={newValue => setEmail(newValue)}
+                 onPressEnter={() => inputPasswordRef.current?.focus()}
+                />
+
+                <InputLogin
+                 label="Senha"
+                 type="password"
+                 value={senha}
+                 ref={inputPasswordRef}
+                 onChange={newValue => setSenha(newValue) }
+                />
+
+                {/* <label>
                     <span>Senha</span>
-                    <input type="password" value={senha} onChange={e => setSenha(e.target.value)}/>
-                </label>
-                
+                    <input
+                        ref={inputPasswordRef}
+                        type="password" value={senha}
+                        onChange={e => setSenha(e.target.value)}
+                    />
+                </label> */}
+
                 <button type="button" onClick={handleEntrar}>
                     Entrar
                 </button>
