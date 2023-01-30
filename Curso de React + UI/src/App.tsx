6615ba008f9f05feede664f3e20fac14528a8cad
vinicius-bastos <viniciusbastos45@gmail.com>
2023-01-30 17:54:14 -0300

    feature: adding react router dom

--- Curso de React + UI/src/App.tsx
+++ Curso de React + UI/src/App.tsx
@@ -1,7 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+
 export const App = () => {
   return (
-      <div className="App">
-        Teste
-      </div>
+    <RouterProvider router={router}/>
   );
 }
--- Curso de React + UI/src/routes/index.tsx
+++ Curso de React + UI/src/routes/index.tsx
@@ -1,1 +1,12 @@
-export {};
+import { createBrowserRouter, Navigate } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+        path: '/pagina-inicial',
+        element: <div>rota teste</div>
+    },
+    {
+        path: '*',
+        element: <Navigate to={'/pagina-inicial'}/>
+    }
+]);
