b9c9d927ecee6a8b38e9c4c24237a9f73aaff13f
vinicius-bastos <viniciusbastos45@gmail.com>
2023-02-02 13:28:14 -0300

    feature: make base layout be responsive

--- Curso de React + UI/src/pages/dashboard/Dashboard.tsx
+++ Curso de React + UI/src/pages/dashboard/Dashboard.tsx
@@ -4,7 +4,7 @@ import { LayoutBaseDePagina } from '../../shared/layouts';
 export const Dashboard = () => {
 
   return (
-    <LayoutBaseDePagina titulo='Pagina Inicial'>
+    <LayoutBaseDePagina titulo='Pagina Inicial' barraDeFerramentas={<>Barra de Ferramentas</>}>
       Testando
     </LayoutBaseDePagina>
   );
